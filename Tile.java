3f431ea7cd816ebeedba8e5f145590690df9e095
Harrison Lee <harrisonl3570@gmail.com>
2025-05-22 10:44:01 -0400

    added Tile constructor and currently adding letterValues

--- Tile.java
+++ Tile.java
@@ -1,5 +1,14 @@
-public class Tile{
-    public static void main(String[] args){
-        
+public class Tile{]
+    Char letter;
+    int letterValue;
+
+    public tile(char assignLetter) {
+      letter = assignLetter;
+      if (letter == 'A' || letter == 'E' || letter == 'I'|| letter == 'O'|| letter == 'U'|| letter == 'L'|| letter == 'N'|| letter == 'S'|| letter == 'T'|| letter == 'R') {
+        letterValue = 1;
+      }
+      else if (letter == 'D'|| letter == 'G') {
+        letterValue = 2;
+      }
     }
 }
