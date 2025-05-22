b35a3b08e3a5de8005b1f6b0c594a93d4fdff8d0
Harrison Lee <harrisonl3570@gmail.com>
2025-05-22 10:49:27 -0400

    finished assigning different letters to their values

--- Tile.java
+++ Tile.java
@@ -10,5 +10,20 @@ public class Tile{]
       else if (letter == 'D'|| letter == 'G') {
         letterValue = 2;
       }
+      else if (letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P') {
+        letterValue = 3;
+      }
+      else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y') {
+        letterValue = 4;
+      }
+      else if (letter = 'K') {
+        letterValue = 5;
+      }
+      else if (letter = 'J' || letter = 'X') {
+        letterValue = 8;
+      }
+      else {
+        letterValue = 10;
+      }
     }
 }
