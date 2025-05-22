3ec92100ff270608cf8d404a3462741719ed1ddb
Harrison Lee <harrisonl3570@gmail.com>
2025-05-22 10:58:14 -0400

    added get methods for tile

--- Tile.java
+++ Tile.java
@@ -1,8 +1,8 @@
-public class Tile{]
-    Char letter;
-    int letterValue;
+public class Tile{
+    private char letter;
+    private int letterValue;
 
-    public tile(char assignLetter) {
+    public Tile(char assignLetter) {
       letter = assignLetter;
       if (letter == 'A' || letter == 'E' || letter == 'I'|| letter == 'O'|| letter == 'U'|| letter == 'L'|| letter == 'N'|| letter == 'S'|| letter == 'T'|| letter == 'R') {
         letterValue = 1;
@@ -16,14 +16,23 @@ public class Tile{]
       else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y') {
         letterValue = 4;
       }
-      else if (letter = 'K') {
+      else if (letter == 'K') {
         letterValue = 5;
       }
-      else if (letter = 'J' || letter = 'X') {
+      else if (letter == 'J' || letter == 'X') {
         letterValue = 8;
       }
-      else {
+      else if (letter == 'Q' || letter == 'Z') {
         letterValue = 10;
       }
     }
+
+    public char getLetter() {
+      return letter;
+    }
+
+    public int getValue() {
+      return letterValue;
+    }
+
 }
