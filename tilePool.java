157d7d17f9e4d4b4cd1afe6ce032d764f7b6786e
krabbypaty <hlee60@stuy.edu>
2025-05-22 23:38:54 -0400

    added a toString for tilePool and tested my constructor

--- tilePool.java
+++ tilePool.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class tilePool {
-    private ArrayList<Tile> tilePool;
+    private ArrayList<Tile> tilePool = new ArrayList<>();
 
     public tilePool() {
         int[] tileFrequency = { 9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1,
                 2, 1 };
-        int ascii = 101;
+        int ascii = 65;
         for (int i = 0; i < 26; i++) {
             for (int frequency = 0; frequency < tileFrequency[i]; frequency++) {
                 Tile tile = new Tile((char) ascii);
@@ -15,5 +15,13 @@ public class tilePool {
             }
             ascii++;
         }
+    }
+
+    public String toString() {
+        char[] letters = new char[tilePool.size()];
+        for (int i = 0; i < tilePool.size(); i++) {
+            letters[i] = tilePool.get(i).getLetter();
+        }
+        return Arrays.toString(letters);
     }
 }
