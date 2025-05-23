1d4bae1e8a4c3a2240e5cda05d43de4d9c3ebee2
krabbypaty <hlee60@stuy.edu>
2025-05-22 23:31:54 -0400

    added tiles into tilepool based on frq

--- tilePool.java
+++ tilePool.java
@@ -1,12 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-public class tilePool.java{
+public class tilePool {
     private ArrayList<Tile> tilePool;
 
     public tilePool() {
-
-    private final int[] tileFrequency = { 9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
-
-}
+        int[] tileFrequency = { 9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1,
+                2, 1 };
+        int ascii = 101;
+        for (int i = 0; i < 26; i++) {
+            for (int frequency = 0; frequency < tileFrequency[i]; frequency++) {
+                Tile tile = new Tile((char) ascii);
+                tilePool.add(tile);
+            }
+            ascii++;
+        }
+    }
 }
