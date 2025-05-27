6e5192ac4c809c681f784814a040862de5ca54ac
Harrison Lee <harrisonl3570@gmail.com>
2025-05-27 10:49:04 -0400

    added constructor for player and vars

--- Player.java
+++ Player.java
@@ -1,5 +1,13 @@
 public class Player{
+  String name;
+  int score;
+  ArrayList<Tile> hand;
+
+  public Player(String giveName) {
+    name = giveName;
+  }
+
     public static void main(String[] args){
-        
+
     }
 }
