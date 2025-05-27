439be88989a13aca72384e6a30ba4061e4a7a538
Amrit Das <amritdas8015@gmail.com>
2025-05-27 10:32:39 -0400

    creating and brainstorming the 2d arrays for the board class

--- Board.java
+++ Board.java
@@ -1,4 +1,10 @@
+import java.util.*;
+import java.io.*;
+
 public class Board{
+    private char[][] board = new char[15][15];
+    private int[][] lettermultipliers = new char[15][15];
+    private int[][] wordmultipliers = new char[15][15];
     public static void main(String[] args){
         
     }
